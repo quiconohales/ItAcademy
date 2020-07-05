986ef7382a1d77eb158002749629bd2908cc7e53
quiconohales <quiconohales@gmail.com>
2020-07-05 21:21:48 +0200

    copia diaria (s'ha creat una clase Fases)

---
+++ exercici4_Restaurant/src/exercici4_Restaurant/Fases.java
@@ -0,0 +1,160 @@
+package exercici4_Restaurant;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Fases {
+
+	
+	int b5=5;int b10=10;int b20=20;int b50=500;int b100=100;int b200=200;int b500=500;
+	int total;
+	String[] menu=new String[5];
+	int[] preus=new int[5];
+	List <Integer> comanda= new ArrayList<>();
+	//creem els scanner per llegir info
+	Scanner scan_plat = new Scanner(System.in);
+	Scanner scan_preu = new Scanner(System.in);
+	Scanner scan_plat_desitjat = new Scanner(System.in);
+	
+public void Fase1() {
+		
+	// FASE 1-------------------------------------------
+	// Inicialitzem variables a nivell de clase per fer-las accessibles des de tota la clase
+	
+	
+	
+	
+	}
+public void Fase2() {
+	
+	// FASE 2-------------------------------------------
+	// recorrem l'array solicitan nom plat i preu. 
+	for (int i = 0; i < menu.length; i++) {
+		
+		System.out.print("Introdueix el nom del plat nº " + (i+1) + ": ");
+		while(!scan_plat.hasNextLine()) scan_plat.next();
+		String plat = scan_plat.nextLine();
+		menu[i]=plat;
+				
+		
+		System.out.print("Introdueix el preu del plat nº " + (i+1) + ": ");
+		while(!scan_preu.hasNextInt()) scan_preu.next();
+		int preu_plat = scan_preu.nextInt();
+		preus[i]=preu_plat;
+		 
+	}
+	System.out.println("");
+	// creem una llista on guardarem els plats escollits
+	// anirem demanan plats mentres no introdueixi 0 per sortir
+	// 
+	int plats_desitjat=99;
+	while(plats_desitjat !=0) {
+		for (int i = 0; i < menu.length; i++) {
+			System.out.println("El plat nº " + (i+1) + " es "+ menu[i]+ " i el seu preu es "+ preus[i]);
+			}
+		System.out.println();
+		System.out.println("Introdueix el nº del plat desitjat o pulse-ho 0 per sortir :");
+		plats_desitjat =scan_plat_desitjat.nextInt();
+		if (plats_desitjat != 0) {
+			comanda.add(plats_desitjat-1);
+			}
+		}
+	// Llistem la llista del plats del menu
+	for (int i = 0; i < menu.length; i++) {
+		System.out.println("El plat nº " + (i+1) + " es "+ menu[i]+ " i el seu preu es "+ preus[i]);
+		}
+	
+	total=0;
+	System.out.println();
+	System.out.println();
+	
+	}
+public void Fase3() {
+
+	
+	// recorrem tota la llsta comanda  i en el cas que el plat introduit no estigui dintre del array de menu mostrem error
+	// en el cas contrari sumem el preu al total  
+	for (int i = 0; i < comanda.size();i++) {
+		
+			if (comanda.get(i) > menu.length) {
+				System.out.println("Error el producte "+ (comanda.get(i)+1) + " no existeix");
+				
+			}else {
+					total=total+preus[comanda.get(i)];
+			} 
+		}
+	// close the scanners and print the total to pay
+	scan_plat_desitjat.close();
+	scan_plat.close();
+	scan_preu.close();
+	System.out.println(" El Total a pagar és :"+ total);
+	
+	
+	
+	
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
